6048d57dbb80b622949e5cfb6fbafeb29ccbcc9b
Riley Paul <rileypaul96@gmail.com>
2023-04-05 22:27:32 -0700

    API boilerplate

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
-from typing import Union
+from src.apis import apis
+from src.prisma import prisma
+from fastapi.middleware.gzip import GZipMiddleware
 from fastapi import FastAPI
 
 app = FastAPI()
+app.add_middleware(GZipMiddleware, minimum_size=1000)
+app.include_router(apis, prefix="/apis")
+
+@app.on_event("startup")
+async def startup():
+    await prisma.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await prisma.disconnect()
+
 
 @app.get("/")
 def read_root():
-  return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-  return {"item_id": item_id, "q": q}
+    return {"version": "1.0.0"}
