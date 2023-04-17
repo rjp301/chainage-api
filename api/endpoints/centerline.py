b99b6b383a0a731b86b7a2d9077f832dec7112f1
Riley Paul <rileypaul96@gmail.com>
2023-04-17 13:46:43 -0600

    add methods for topcon runs

--- api/endpoints/centerline.py
+++ api/endpoints/centerline.py
@@ -1,14 +1,21 @@
 from fastapi import APIRouter
 from ..utils.prisma import prisma
 from pydantic import BaseModel
+from typing import List
 
 router = APIRouter(prefix="/centerline")
+
+class CenterlineMarker(BaseModel):
+  value: float
+  x: float
+  y: float
 
 class Centerline(BaseModel):
   userId: int
   name: str
   description: str = ""
   line: str
+  markers: List[CenterlineMarker]
 
 @router.get("/")
 async def all_centerlines():
@@ -16,11 +23,25 @@ async def all_centerlines():
 
 @router.get("/{centerline_id}")
 async def get_centerline(centerline_id:int):
-  return await prisma.centerline.find_unique(where={"id":centerline_id})
+  return await prisma.centerline.find_unique(
+    where={"id":centerline_id},
+    include={"markers": True}
+  )
 
 @router.post("/")
 async def create_centerline(centerline:Centerline):
-  return await prisma.centerline.create(centerline)
+  return await prisma.centerline.create(
+    data={
+      "userId": centerline.userId,
+      "name": centerline.name,
+      "description": centerline.description,
+      "line": centerline.line,
+      "markers": {
+        "create": [{"value":marker.value, "x":marker.x, "y":marker.y} for marker in centerline.markers]
+      }
+    },
+    include={"markers":True}
+  )
 
 @router.delete("/{centerline_id}")
 async def delete_centerline(centerline_id:int):
--- api/endpoints/topcon.py
+++ api/endpoints/topcon.py
@@ -1,15 +1,51 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File, Form
 from ..utils.prisma import prisma
+from pydantic import BaseModel
+from typing import Annotated
 
 router = APIRouter(prefix="/topcon")
 
+class TopconInfo(BaseModel):
+  width_bot: float
+  slope: float
+
+@router.post("/")
+async def run_topcon(
+  width_bot: Annotated[float,Form()],
+  slope: Annotated[float,Form()],
+  centerlineId: Annotated[int,Form()],
+  ground_csv: UploadFile = File(...),
+  ditch_shp: UploadFile = File(...),
+):
+  
+  # Perform topcon calculation here including calculating KP of points
+  # Transform ditch_shp
+  # Transform ground_csv
+  # Calculate height and area for each point
+  # Convert points to ranges
+  # Calculate volumes for each range
+  # Save all pertinent info to db
+  # Return data_pts and data_rng and KP string
+
+  return {
+    "width_bot": width_bot,
+    "slope": slope,
+    "ground_csv": ground_csv.filename,
+    "ditch_shp": ditch_shp.filename
+  }
+
+
+@router.get("/")
+async def all_topcon_runs():
+  return await prisma.topconrun.find_many()
+
+
 @router.get("/{run_id}")
 async def get_run(run_id: int):
-  run = await prisma.topconrun.find_unique(
-    where={ "id": run_id },
-    include={ "data_pts": True, "data_rng": True }
-  )
-  return run
+  run = await prisma.topconrun.find_unique(where={ "id": run_id })
+  data_rng = await prisma.topcondatarng.find_unique(where={"runId": run_id})
+  data_pts = await prisma.topcondatapts.find_unique(where={"runId": run_id})
+  return {"info": run, "data_pts": data_pts, "data_rng": data_rng}
 
 
 @router.get("/download/{run_id}")
