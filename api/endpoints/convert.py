2f563907b3d58e340f823d63fd39f69569b43f03
Riley Paul <rileypaul96@gmail.com>
2023-04-17 08:30:22 -0600

    get json to excel endpoint working

--- api/endpoints/convert.py
+++ api/endpoints/convert.py
@@ -1,13 +1,12 @@
-from fastapi import UploadFile, File, HTTPException
-from fastapi.responses import StreamingResponse
+from fastapi import UploadFile, File, HTTPException, Depends
+from fastapi.responses import FileResponse
 from fastapi import APIRouter
 
 import json
 import shutil
 import shapefile
-
-from tempfile import TemporaryDirectory
-from tempfile import NamedTemporaryFile
+import openpyxl
+import tempfile
 
 from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
@@ -17,57 +16,39 @@ from pathlib import Path
 
 router = APIRouter()
 
+def create_excel_workbook(file_path, worksheet_data):
+  wb = openpyxl.Workbook()
+  for sheet_name, sheet_data in worksheet_data.items():
+    sheet = wb.create_sheet(sheet_name)
+    for row_idx, row_data in enumerate(sheet_data):
+      for col_idx, col_data in enumerate(row_data):
+        sheet.cell(row=row_idx+1, column=col_idx+1, value=col_data)
+  wb.save(file_path)
+  return wb
+
+async def get_temp_dir():
+  fname = tempfile.NamedTemporaryFile(suffix=".xlsx")
+  try:
+    yield fname.name
+  finally:
+    del fname
+
+
 @router.post("/json-to-excel")
-async def json_to_excel(data: Dict[str, List[Dict[str, str]]]):
-  # Validate the JSON payload
-  if not isinstance(data, dict):
-    raise HTTPException(status_code=400, detail="Invalid JSON payload: expected a dictionary")
-
-  for ws_name, rows in data.items():
-    if not isinstance(ws_name, str):
-      raise HTTPException(status_code=400, detail=f"Invalid worksheet name: {ws_name}")
-
-    if not isinstance(rows, list):
-      raise HTTPException(status_code=400, detail=f"Invalid data rows for worksheet {ws_name}: expected a list")
-
-    if not all(isinstance(row, dict) for row in rows):
-      raise HTTPException(status_code=400, detail=f"Invalid data rows for worksheet {ws_name}: expected a list of dictionaries")
-
-  # Create a new Excel workbook
-  wb = Workbook()
-
-  # Loop through each worksheet in the JSON payload
-  for ws_name, rows in data.items():
-    # Create a new worksheet in the workbook
-    ws = wb.create_sheet(title=ws_name)
-
-    # Add the column headers to the first row of the worksheet
-    headers = rows[0].keys()
-    for col_num, header in enumerate(headers, 1):
-      col_letter = get_column_letter(col_num)
-      ws[f"{col_letter}1"] = header
-
-    # Add the data rows to the worksheet
-    for row_num, row_data in enumerate(rows, 2):
-      for col_num, cell_data in enumerate(row_data.values(), 1):
-        col_letter = get_column_letter(col_num)
-        ws[f"{col_letter}{row_num}"] = cell_data
-
-  # Write the workbook to a temporary file
-  with NamedTemporaryFile(delete=True) as tmp:
-    wb.save(tmp.name)
-    tmp.seek(0)
-
-    # Return the temporary file as a StreamingResponse with the appropriate content type
-    return StreamingResponse(tmp, 
-      media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
-      headers={"Content-Disposition": "attachment; filename=export.xlsx"}
-    )
+async def json_to_excel(data: dict, fname=Depends(get_temp_dir)):
+  filename = data["filename"] + ".xlsx"
+  worksheet_data = data["worksheets"]
+ 
+  create_excel_workbook(fname, worksheet_data)
+  return FileResponse(fname, 
+    media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", 
+    filename=filename
+  )
 
 
 @router.post("/shapefile-to-geojson")
 async def shapefile_to_geojson(file: UploadFile = File(...)):
-  with TemporaryDirectory() as temp_dir:
+  with tempfile.TemporaryDirectory() as temp_dir:
     # Save uploaded file to temporary directory
     file_path = Path(temp_dir) / file.filename
     with file_path.open("wb") as buffer:
